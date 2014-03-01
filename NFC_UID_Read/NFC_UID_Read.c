0267aa05c287c673f748386a5964e8805d337e4b
Pierre Pelé <pp.pele.pierre@gmail.com>
2014-03-01 18:00:12 +0100

    01/03/2014 17H59 - Pierre Pelé
    Suppression des commenatires

--- NFC_UID_Read/NFC_UID_Read.c
+++ NFC_UID_Read/NFC_UID_Read.c
@@ -4,58 +4,38 @@
 static void print_hex(const uint8_t *pbtData, const size_t szBytes)
 {
     size_t  szPos;
-    
     for (szPos = 0; szPos < szBytes; szPos++)
     {
         printf("%02x  ", pbtData[szPos]);
     }
     printf("\n");
 }
-
 int main(int argc, const char *argv[])
 {
     nfc_device *pnd;
     nfc_target nt;
-    
-    // Allocate only a pointer to nfc_context
     nfc_context *context;
-    
-    // Initialize libnfc and set the nfc_context
     nfc_init(&context);
     if (context == NULL)
     {
         printf("Unable to init libnfc (malloc)\n");
         exit(EXIT_FAILURE);
     }
-    
-    // Display libnfc version
     const char *acLibnfcVersion = nfc_version();
     (void)argc;
     printf("%s uses libnfc %s\n", argv[0], acLibnfcVersion);
-    
-    // Open, using the first available NFC device which can be in order of selection:
-    //   - default device specified using environment variable or
-    //   - first specified device in libnfc.conf (/etc/nfc) or
-    //   - first specified device in device-configuration directory (/etc/nfc/devices.d) or
-    //   - first auto-detected (if feature is not disabled in libnfc.conf) device
-    
     pnd = nfc_open(context, NULL);
-    
     if (pnd == NULL)
     {
         printf("ERROR: %s\n", "Unable to open NFC device.");
         exit(EXIT_FAILURE);
     }
-    // Set opened NFC device to initiator mode
     if (nfc_initiator_init(pnd) < 0)
     {
         nfc_perror(pnd, "nfc_initiator_init");
         exit(EXIT_FAILURE);
     }
-    
     printf("Lecteur NFC : %s ouvert\n", nfc_device_get_name(pnd));
-    
-    // Poll for a ISO14443A (MIFARE) tag
     const nfc_modulation nmMifare =
     {
         .nmt = NMT_ISO14443A,
@@ -85,9 +65,7 @@ int main(int argc, const char *argv[])
         }
         printf("\n");
     }
-    // Close NFC device
     nfc_close(pnd);
-    // Release the context
     nfc_exit(context);
     exit(EXIT_SUCCESS);
 }
